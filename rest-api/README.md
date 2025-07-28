c002509e59a1ecfad586ea7f2f8ab330d608e2be
Om Balapure <odbalapure@gmail.com>
2025-07-28 21:48:07 +0530

    REST API setup

---
+++ rest-api/README.md
@@ -0,0 +1,47 @@
+## REST API
+
+- [DB functions](./cmd/api/db.go)
+- [Setting up routes](./cmd/api/routes.go)
+- [Starting the app and db server](./cmd/api/main.go)
+
+### Creating a REST endpoint in Go
+
+Inside the routes.go file, add a new route the following way
+
+```go
+mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+    var payload = struct {
+        Message string `json:"message"`
+    }{
+        Message: "Hello world!",
+    }
+
+    _ = app.writeJSON(w, http.StatusOK, payload)
+})
+```
+
+The writeJSON is a parses the data passed to it into JSON format
+
+
+```go
+var out []byte
+// ...
+jsonBytes, err := json.Marshal(data)
+if err != nil {
+    return err
+}
+out = jsonBytes
+
+w.Header().Set("Content-Type", "application/json")
+w.WriteHeader(status)
+
+_, err := w.Write(out)
+if err != nil {
+    return err
+}
+return nil
+// ...
+```
+
+## Getting started with JWT auth
+
---
+++ rest-api/cmd/api/routes.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// register middleware
+	mux.Use(middleware.Recoverer)
+	// mux.Use(app.enableCORS)
+
+	// authentication routes - authenticate, refresh
+	mux.Post("/auth", app.authenticate)
+	mux.Post("/refresh", app.refresh)
+
+	// test handler
+	mux.Get("/test", func(w http.ResponseWriter, r *http.Request) {
+		var payload = struct {
+			Message string `json:"message"`
+		}{
+			Message: "Hello world!",
+		}
+
+		_ = app.writeJSON(w, http.StatusOK, payload)
+	})
+
+	// protected routes
+	mux.Route("/users", func(mux chi.Router) {
+		// auth middleware
+		mux.Get("/", app.allUsers)
+		mux.Get("/{userID}", app.getUser)
+		mux.Delete("/{userID}", app.deleteUser)
+		mux.Put("/", app.insertUser)
+		mux.Patch("/", app.updateUser)
+	})
+
+	return mux
+}
